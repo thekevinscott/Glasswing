d5ff1c962f1b1e2ee297503e5fd49725ad436033
Kevin Scott <kevin@scottlabsllc.com>
2013-06-26 17:22:58 -0400

    Allow save without a server

--- js/setup.js
+++ js/setup.js
@@ -2,6 +2,9 @@
 
   // document ready
   $(function() {
+    //Overrides persistence storage with dummy function. This enables use of Model.destroy() without raising an error.
+    Backbone.sync = function(method, model, success, error){success();}
+
     // Set up some options for jQuery and plugins.
     $(document).ajaxError(function() {
       alert("There was an error.");
@@ -11,6 +14,11 @@
     var patient1 = new glasswing.models.patient({firstname : "Bob", lastname: "Kraut"});
 
     var patient2 = new glasswing.models.patient({first : "Bob", last: "Kraut"});
+
+
+    glasswing.collections.Patients = Backbone.Collection.extend({
+      model: glasswing.models.patient
+    });
 
     // Backbone.emulateJSON = true;
 
