b6c8ad1187e70513c3ec039027933685984ecd63
Kevin Scott <kevin@scottlabsllc.com>
2013-06-29 12:33:07 -0400

    styling report page and adding readme

--- js/views/report.js
+++ js/views/report.js
@@ -10,7 +10,7 @@ define([
 		// model : new worklist(),
 		template : template,
 		events : {
-		  // "click tbody tr" : "openProcedure"
+		  "click .actions input[type=button]" : "saveProcedure"
 		},
 		initialize : function(attributes) {
 
@@ -26,10 +26,37 @@ define([
 		setOptions : function(options) {
 
 		},
+		saveProcedure : function(event) {
+			var button = $(event.currentTarget);
 
+			switch(button.val()) {
+				case 'Read' :
+				break;
+				case 'Co-Read' :
+				break;
+				case 'Approve' :
+				break;
+			}
+
+			window.location.hash = 'worklist';
+			// how do I access tab manager in this scenario?
+		},
 		render : function() {
+
 			this.$el.html(_.template(this.template, {
-				name : this.model.get('name')
+				procedure_id : this.model.get('procedure_id'),
+				patient_id : this.model.get('patient').get('patient-id'),
+				patient_risks : this.model.get('patient').get('patient-risks'),
+				dob : this.model.get('patient').get('dob'),
+				gender : this.model.get('patient').get('gender'),
+				name : this.model.get('name'),
+				priority : this.model.get('priority'),
+				procedure_class : this.model.get('procedure_class'),
+				procedure_name : this.model.get('procedure_name'),
+				report_status : this.model.get('report_status'),
+				hospital_name : this.model.get('hospital_name'),
+				referring_physician : this.model.get('referring_physician'),
+
 			}));
 			return this;
 		}
