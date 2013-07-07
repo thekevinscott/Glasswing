e2783289a618217ca1c80ed9caeb96354634e030
Kevin Scott <kevin@scottlabsllc.com>
2013-07-07 10:45:09 -0400

    minor tweaks to caregiver screen

--- js/lib/jquery.modal.js
+++ js/lib/jquery.modal.js
@@ -1,11 +1,38 @@
 (function($){
-	$.fn.modal = function() {
-		var content = $('<div class="content" />');
+	$.fn.modal = function(options) {
+		var self = $(this);
+		var button = $(options.button);
+
+
+
+
+		$('body').append(self);
+
+		var content = $(self).wrap('<div class="modal-content" />').parent();
+		var modal = content.wrap('<div class="modal" />').parent();
 		var arrow = $('<div class="arrow" />');
-		var modal = $('<div class="modal" />');
-		$(this).wrap(content);
-		content.wrap(modal);
-		content.before(arrow);
-		return modal;
+		var close = $('<a href="javascript:;" class="close" />');
+
+		modal.prepend(arrow);
+		modal.prepend(close);
+		var overlay = $('<div class="modal-overlay" />');
+		modal.before(overlay);
+
+		var exit = function() {
+			overlay.remove();
+			modal.remove();
+		}
+		overlay.click(exit);
+		close.click(exit);
+
+
+
+		// var width = button.width();
+		// var left = ($(button).position().left - 30 - width/2 ) + width/2;
+		var left = button.position().left - (modal.outerWidth() / 2) + (button.width() / 2);
+		var top = button.position().top + (button.outerHeight() + arrow.outerHeight());
+		modal.css({left: left+'px', top: top});
+
+
 	}
 })(jQuery);
